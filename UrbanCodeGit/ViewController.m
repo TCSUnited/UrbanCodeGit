dfd0faf11c43ff8de9df70cebe58aaab266fd675
Ankit <tcs.unitedairlines@gmail.com>
2017-03-29 16:27:35 +0530

    Revert "Added UI Testing"
    This reverts commit efb1f309b62e4107b78b6a524eb5b7a2998de8e2.

--- UrbanCodeGit/ViewController.m
+++ UrbanCodeGit/ViewController.m
@@ -14,7 +14,6 @@
 
 @implementation ViewController
 
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -36,13 +35,4 @@
     
 }
 
-- (IBAction)copyText:(id)sender {
-    
-    
-    [_destinationBtn setTitle:_textField.text forState:UIControlStateNormal];
-    
-}
-
-- (IBAction)destinationButton:(id)sender {
-}
 @end
