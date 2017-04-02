ece8e8a032f6861c843bf50a1ccc1ccf7cdd8a76
TCSUnited <UnitedTCS@Uniteds-iMac.local>
2017-04-02 19:24:53 +0530

    Ui testing right

--- UrbanCodeGitUITests/UrbanCodeGitUITests.m
+++ UrbanCodeGitUITests/UrbanCodeGitUITests.m
@@ -42,7 +42,7 @@
     XCUIApplication *app = [[XCUIApplication alloc] init];
     XCUIElement *textField = [[app.otherElements containingType:XCUIElementTypeButton identifier:@"Copy the contetnt"] childrenMatchingType:XCUIElementTypeTextField].element;
     [textField tap];
-    [textField typeText:@"hello hi"];
+    [textField typeText:@"hello"];
     [app.buttons[@"Copy the contetnt"] tap];
     XCTAssert(app.buttons[@"hello"].exists);
     
